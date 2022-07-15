ef53e33a9ca3faa5b0c5d9b837e1a68ecbd16eff
vivian <vivian.v18723@gmail.com>
2022-07-15 11:50:26 +0100

    add vegan key

--- server/data.js
+++ server/data.js
@@ -2,24 +2,28 @@ const data = [
   {
     id: 1,
     flavour: "pistachio",
+    vegan: false,
   },
   {
     id: 2,
     flavour: "salted caramel",
+    vegan: false,
   },
   {
     id: 3,
     flavour: "lemon sorbet",
+    vegan: false,
   },
   {
     id: 4,
     flavour: "vanilla",
+    vegan: true,
   },
   {
     id: 5,
     flavour: "mint",
+    vegan: true,
   },
 ];
 
 module.exports = data;
-
