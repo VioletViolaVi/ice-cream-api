114c2f9e80b3deac051c7d2f194334a153d3a95f
vivian <vivian.v18723@gmail.com>
2022-07-15 11:29:02 +0100

    add data object

---
+++ server/data.js
@@ -0,0 +1,25 @@
+const data = [
+  {
+    id: 1,
+    flavour: "pistachio",
+  },
+  {
+    id: 2,
+    flavour: "salted caramel",
+  },
+  {
+    id: 3,
+    flavour: "lemon sorbet",
+  },
+  {
+    id: 4,
+    flavour: "vanilla",
+  },
+  {
+    id: 5,
+    flavour: "mint",
+  },
+];
+
+module.exports = data;
+
