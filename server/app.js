089c7a0ff2992b09d1619242f3d74dbc88a47cb0
vivian <vivian.v18723@gmail.com>
2022-07-15 10:03:16 +0100

    add notes

--- server/app.js
+++ server/app.js
@@ -1,5 +1,13 @@
 const express = require("express");
 
+// creates express app
 const app = express();
 
-app.get("/");
+// shows what type of request being made & url where request is being made & response will be gotten
+
+// sets up route
+app.get("/", (request, response) => {
+  response.send("Welcome to the Ice Cream API");
+});
+
+module.exports = app;
